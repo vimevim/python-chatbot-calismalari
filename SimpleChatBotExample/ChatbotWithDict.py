485f90c1024303b93b5e395630dd36dc8e8db9ed
Hasan Sezer Taşan <vimevim@users.noreply.github.com>
2020-11-14 01:26:35 +0300

    method düzeltmesi

--- SimpleChatBotExample/ChatbotWithDict.py
+++ SimpleChatBotExample/ChatbotWithDict.py
@@ -1,15 +1,33 @@
+import string
 inputOutputDict = {
     "merhaba" :"Merhaba, hoşgeldiniz.",
     "hoş buldum" : "",#tepkisiz
     "hoş bulduk" : "",
     "nasılsın" : "İyiyim efendim, siz nasılsınız?",
     "iyiyim" : "Bunu duyduğuma sevindim.",
-    "2+2 kaç eder" :"4 eder efendim",
+    "2 artı 2 kaç eder" :"4 eder efendim",
     "ankarada hava durumu ne" : "Soğuk, brrrrr.",
     "adın ne" : "Benim adım Ayelar",
     "ismin ne" :"adın ne",#aynı soruların cevaplarını eşleştir
     "senin adın ne" : "adın ne",
 }
+def ciktiAl(girdi):
+    # kullanıcı girdisinin dictionary içerisinde var mı kontrol ediyoruz
+    if (girdi in inputOutputDict):
+        # kullanıcı girsininin karşılığı var mı kontrol ediyoruz
+        if (inputOutputDict[girdi] != ""):
+            # kullanıcı girdisinin karşılığı var ama karşılık dictionary içerisinde var mı kontrol ediyoruz
+            if (inputOutputDict[girdi] not in inputOutputDict):
+                print(inputOutputDict[girdi])
+            # kullanıcı girdisinin karşılığı dictionary içerisinde yoksa, karşılığı kullanıcıya veriyoruz
+            else:
+                print(inputOutputDict[inputOutputDict[girdi]])
+        # kullanıcı girdisinin karşılığı yoksa pass geçiyoruz
+        else:
+            pass
+    # kullanıcı girdisi dictionary içerisinde yoksa "ne dediğinizi anlamadım" mesajı veriyoruz
+    else:
+        print("Ne dediğinizi anlamadım")
 # diyalogu sonsuz döngüye sokuyoruz
 while(True):
     # kullanıcıdan girdi alıyoruz
@@ -17,22 +35,5 @@ while(True):
     # kullanıcı girdisini küçük harfe çeviriyoruz
     userInput=userInput.lower()
     # kullanıcı girdisinde ki noktalama işaretlerini yok ediyoruz
-    userInput=userInput.replace(".","")
-    userInput=userInput.replace("!","")
-    userInput=userInput.replace("?","")
-    # kullanıcı girdisinin dictionary içerisinde var mı kontrol ediyoruz
-    if(userInput in inputOutputDict):
-        # kullanıcı girsininin karşılığı var mı kontrol ediyoruz
-        if(inputOutputDict[userInput] !=""):
-            #kullanıcı girdisinin karşılığı var ama karşılık dictionary içerisinde var mı kontrol ediyoruz
-            if (inputOutputDict[userInput] in inputOutputDict):
-                print(inputOutputDict[inputOutputDict[userInput]])
-            #kullanıcı girdisinin karşılığı dictionary içerisinde yoksa, karşılığı kullanıcıya veriyoruz
-            else:
-                print(inputOutputDict[userInput])
-        #kullanıcı girdisinin karşılığı yoksa pass geçiyoruz
-        else:
-            pass
-    #kullanıcı girdisi dictionary içerisinde yoksa "ne dediğinizi anlamadım" mesajı veriyoruz
-    else:
-        print("Ne dediğinizi anlamadım")
+    userInput = "".join([c for c in userInput if c in string.digits + string.ascii_letters + "ÖÜÇŞĞöüçşıİğ "])
+    ciktiAl(userInput)
